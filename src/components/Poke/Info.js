3e065dd8f8b8aa7e1c33bba99b1ba877d24d5854
lBastianl-CL <bastianfloresgonzalez@gmail.com>
2022-09-04 23:04:32 -0300

    Clean and order code

--- src/components/Poke/Info.js
+++ src/components/Poke/Info.js
@@ -11,35 +11,35 @@ const Info = ({
 }) => {
   return (
     <>
-    <div className="container-search mb-4">
-      <Form.Label>Características del Pokemon</Form.Label>
+      <div className="container-search mb-4">
+        <Form.Label>Características del Pokemon</Form.Label>
       </div>
-    <div className="container-info d-flex flex-wrap my-4">
-      <div className="info-item">
-        <h4>Altura</h4>
-        <p>{Math.round(height * 10) / 100} m</p>
-      </div>
+      <div className="container-info d-flex flex-wrap my-4">
+        <div className="info-item">
+          <h4>Altura</h4>
+          <p>{Math.round(height * 10) / 100} m</p>
+        </div>
 
-      <div className="info-item">
-        <h4>Porcentaje de captura</h4>
-        <p>{Math.round(capture_rate * 100) / 100}%</p>
-      </div>
+        <div className="info-item">
+          <h4>Porcentaje de captura</h4>
+          <p>{Math.round(capture_rate * 100) / 100}%</p>
+        </div>
 
-      <div className="info-item">
-        <h4>Peso</h4>
-        <p>{Math.round(weight * 10) / 100} kg</p>
-      </div>
+        <div className="info-item">
+          <h4>Peso</h4>
+          <p>{Math.round(weight * 10) / 100} kg</p>
+        </div>
 
-      <div className="info-item">
-        <h4>Habilidades</h4>
-        <p>{abilities != null ? abilities : "-"}</p>
-      </div>
+        <div className="info-item">
+          <h4>Habilidades</h4>
+          <p>{abilities != null ? abilities : "-"}</p>
+        </div>
 
-      <div className="info-item mb-0">
-        <h4>Hábitat</h4>
-        <p>{habitat != null ? habitat : "-"}</p>
-      </div>
-    </div></>
+        <div className="info-item mb-0">
+          <h4>Hábitat</h4>
+          <p>{habitat != null ? habitat : "-"}</p>
+        </div>
+      </div></>
   );
 };
 
--- src/layouts/Header/Header.js
+++ src/layouts/Header/Header.js
@@ -1,6 +1,6 @@
 import React from "react";
-import {Container} from "react-bootstrap";
-import {Link} from "react-router-dom";
+import { Container } from "react-bootstrap";
+import { Link } from "react-router-dom";
 import logo from "../../assets/images/logo.png";
 import "./styles.css";
 
